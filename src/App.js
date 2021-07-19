3685f1924242b8cffc62840977d037630d4af042
samon03 <samon3123@gmail.com>
2021-07-19 22:22:09 +0600

    messege

--- src/App.js
+++ src/App.js
@@ -31,7 +31,10 @@ function App() {
   return (
     <div className="container">
        <Header title="Task Tracker"/>
-       <Tasks tasks={tasks}  onDelete={deleteTask} />
+       { tasks.length > 0 ?
+          <Tasks tasks={tasks}  onDelete={deleteTask} />
+          : "No task to show"
+       }
     </div>
   );
 }
