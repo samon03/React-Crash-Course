d7823e49a3170b127a2bffe7a5bee83046fa7ae3
samon03 <samon3123@gmail.com>
2021-07-26 22:03:56 +0600

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Header from './components/Header';
 import Tasks from './components/Tasks';
@@ -9,14 +9,29 @@ import './App.css';
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
 
-  const [tasks, setTask] = useState([]);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async() => {
+      const taskFromServer = await fetchTask();
+      setTasks(taskFromServer);
+    }
+    getTasks();
+  }, [])
+
+  const fetchTask = async() => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+
+    return data;
+  }
 
   const deleteTask = (id) => {
-    setTask(tasks.filter((task) => task.id !== id));
+    setTasks(tasks.filter((task) => task.id !== id));
   }
 
   const reminder = (id) => {
-    setTask(tasks.map((task) => 
+    setTasks(tasks.map((task) => 
        task.id === id ? { ...task, reminder: !task.reminder } : task
       )
     )
@@ -30,7 +45,7 @@ function App() {
 
     //add to tasks
 
-    setTask([...tasks, newTask]);
+    setTasks([...tasks, newTask]);
 
   }
 
